b2acfda04d19e37a8222bd51729cf8db80d878fe
souhype <souhype@protonmail.com>
2022-10-22 16:58:19 +0200

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Sprite {
     constructor(parameter) {
         this.position = {
             x: 0,
-            y: 0,
+            y: parameter.height || 0,
         };
         this.image = new Image();
         this.animations = parameter.animations;
@@ -47,10 +47,9 @@ class Entity extends Sprite {
         this.keyPressed = {
             d: false,
             a: false,
+            w: false,
         };
-
-        this.friction = 0.99;
-        this.speed = 0.1;
+        this.friction = 0.9;
         this.hitbox = parameter.hitbox || {
             width: 0,
             height: 0,
@@ -58,45 +57,72 @@ class Entity extends Sprite {
             y: 0,
         };
     }
+    collision(width, height) {
+        this.onGround = false;
+
+        if (this.position.x + this.hitbox.x < 0) this.position.x = -this.hitbox.x;
+        if (this.position.x + this.hitbox.x + this.hitbox.width > width)
+            this.position.x = width - this.hitbox.x - this.hitbox.width;
+
+        if (this.position.y + this.hitbox.y + this.hitbox.height > height) {
+            this.position.y = height - this.hitbox.y - this.hitbox.height;
+            this.onGround = true;
+        }
+    }
     movement() {
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-        console.log(this.velocity.x);
+        this.position.x += this.velocity.x *= this.friction;
+        this.position.y += this.velocity.y *= this.friction;
 
-        this.velocity.x *= this.friction;
-        this.velocity.y *= this.friction;
+        if (!this.onGround) this.velocity.y += this.friction;
 
-        if (this.keyPressed.d) this.velocity.x += this.speed;
-        if (this.keyPressed.a) this.velocity.x -= this.speed;
+        if (this.keyPressed.w && this.onGround) this.velocity.y -= this.friction * 40;
+
+        if (this.keyPressed.d) this.velocity.x += this.friction;
+        if (this.keyPressed.a) this.velocity.x -= this.friction;
+
+        if (!(this.keyPressed.d || this.keyPressed.a)) this.velocity.x *= this.friction * 0.1;
     }
     stateManager() {
-        if (this.velocity.x > this.friction || this.velocity.x < -this.friction) this.state = 'run';
-        else this.state = 'idle';
+        this.state = 'idle';
+        if (this.onGround) {
+            if (this.velocity.x > this.friction || this.velocity.x < -this.friction) {
+                this.state = 'run';
+                console.log('runnin');
+            }
+        }
+        if (!this.onGround) {
+            if (this.velocity.y < 0) this.state = 'jump';
+            else this.state = 'fall';
+        }
     }
-
     input() {
         onkeydown = event => {
+            if (event.key === 'w') this.keyPressed.w = true;
             if (event.key === 'd') this.keyPressed.d = true;
             if (event.key === 'a') this.keyPressed.a = true;
         };
         onkeyup = event => {
-            this.velocity.x *= this.speed;
+            if (event.key === 'w') this.keyPressed.w = false;
             if (event.key === 'd') this.keyPressed.d = false;
             if (event.key === 'a') this.keyPressed.a = false;
         };
     }
-    update(context) {
-        // context.fillStyle = 'hsl(0 50% 50%/1)';
-        // context.fillRect(
-        //     this.position.x + this.hitbox.x,
-        //     this.position.y + this.hitbox.y,
-        //     this.hitbox.width,
-        //     this.hitbox.height
-        // );
-        super.update(context);
+    drawHitbox(context) {
+        context.fillStyle = 'hsl(0 0% 100%/0.1)';
+        context.fillRect(
+            this.position.x + this.hitbox.x,
+            this.position.y + this.hitbox.y,
+            this.hitbox.width,
+            this.hitbox.height
+        );
+    }
+    update(context, width, height) {
+        this.drawHitbox(context);
         this.stateManager();
         this.input();
         this.movement();
+        this.collision(width, height);
+        super.update(context);
     }
 }
 class Game {
@@ -107,13 +133,14 @@ class Game {
         this.context.canvas.height = 450;
 
         this.player = new Entity({
+            height: this.context.canvas.height,
+            scaleFactor: 2,
             hitbox: {
                 width: 40,
                 height: 70,
                 x: 30,
-                y: 17,
+                y: 20,
             },
-            scaleFactor: 2,
             animations: {
                 idle: {
                     imageSrc: 'idle.png',
@@ -127,14 +154,24 @@ class Game {
                     counter: 0,
                     max: 8,
                 },
+                fall: {
+                    imageSrc: 'fall.png',
+                    current: 0,
+                    counter: 0,
+                    max: 3,
+                },
+                jump: {
+                    imageSrc: 'jump.png',
+                    current: 0,
+                    counter: 0,
+                    max: 3,
+                },
             },
         });
     }
-
     run = () => {
-        this.context.fillStyle = 'hsl(0 0% 0%/0.6)';
-        this.context.fillRect(0, 0, this.context.canvas.width, this.context.canvas.height);
-        this.player.update(this.context);
+        this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+        this.player.update(this.context, this.context.canvas.width, this.context.canvas.height);
         requestAnimationFrame(this.run);
     };
 }
